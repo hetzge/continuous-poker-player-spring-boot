7cec0d48e8ad6c07c31e8dc7538c8a46c2c1a60e
Markus Hettich <markus.hettich@doubleslash.de>
2023-02-23 20:06:46 +0100

    Pairs to the top

--- src/main/java/org/continuouspoker/player/logic/PokerBot.java
+++ src/main/java/org/continuouspoker/player/logic/PokerBot.java
@@ -1,6 +1,12 @@
 package org.continuouspoker.player.logic;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.continuouspoker.player.model.Bet;
+import org.continuouspoker.player.model.Rank;
 import org.continuouspoker.player.model.Table;
 
 public class PokerBot {
@@ -12,7 +18,20 @@ public class PokerBot {
       System.out.println(table);
       this.table = new PokerTable(table);
       this.player = this.table.getPlayer();
-      return createRaiseBet();
+
+      final Map<Rank, List<PokerCard>> pairs = Stream
+              .concat(this.table.getCards().stream(), this.player.getCards().stream())
+              .collect(Collectors.groupingBy(card -> {
+                 return card.card.getRank();
+              })).entrySet().stream().filter(entry -> entry.getValue().size() > 1).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+      if(pairs.size() > 0) {
+         System.out.println("Pair!!!");
+         return createAllIn();
+      } else {
+         System.out.println("...");
+         return createRaiseBet();
+      }
    }
 
    public boolean canCheck() {
@@ -21,6 +40,10 @@ public class PokerBot {
 
    public Bet createCheckBet() {
       return new Bet().bet(0);
+   }
+
+   public Bet createAllIn() {
+      return new Bet().bet(player.player.getStack());
    }
 
    public Bet createRaiseBet() {
--- src/main/java/org/continuouspoker/player/logic/PokerCard.java
+++ src/main/java/org/continuouspoker/player/logic/PokerCard.java
@@ -1,5 +1,7 @@
 package org.continuouspoker.player.logic;
 
+import java.util.Objects;
+
 import org.continuouspoker.player.model.Card;
 
 public class PokerCard {
@@ -8,6 +10,24 @@ public class PokerCard {
 
     public PokerCard(final Card card) {
         this.card = card;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final PokerCard pokerCard = (PokerCard) o;
+        return Objects.equals(card, pokerCard.card);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(card);
     }
 
 }
