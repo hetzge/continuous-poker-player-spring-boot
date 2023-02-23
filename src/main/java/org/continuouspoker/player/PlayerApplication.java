2e171a87eb480403343f04bff45a107c06eb1b47
Markus Hettich <markus.hettich@doubleslash.de>
2023-02-23 19:49:20 +0100

    Raise to the top

--- src/main/java/org/continuouspoker/player/PlayerApplication.java
+++ src/main/java/org/continuouspoker/player/PlayerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlayerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Lets go");
 		SpringApplication.run(PlayerApplication.class, args);
 	}
 }
